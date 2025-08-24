f65d56e948dc923b24527146eac08bac7a2de240
Dakshraj44 <153524558+Dakshraj44@users.noreply.github.com>
2025-08-24 10:35:57 +0530

    Update env.js

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -2,8 +2,8 @@ import "dotenv/config" ;
 
 export const ENV = {
     PORT : process.env.PORT || 5001,
-    MONGODB_URI : process.env.MONGODB_URI,
-    NODE_ENV : process.env.NODE_ENV,
+    MONGODB_URI:process.env.MONGODB_URI,
+    NODE_ENV:process.env.NODE_ENV,
     CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
     STREAM_API_KEY:process.env.STREAM_API_KEY,
