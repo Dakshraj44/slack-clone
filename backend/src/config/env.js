5548ce12cd2c19c5011e290ab527a1c7269d5ee4
Dakshraj44 <daksh072005@gmail.com>
2025-08-24 14:53:44 +0530

    little

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,9 +1,9 @@
 import "dotenv/config" ; 
 
 export const ENV = {
-    PORT : process.env.PORT || 5001,
-    MONGODB_URI : process.env.MONGODB_URI,
-    NODE_ENV : process.env.NODE_ENV,
+    PORT:process.env.PORT || 5001,
+    MONGODB_URI:process.env.MONGODB_URI?.trim(),
+    NODE_ENV:process.env.NODE_ENV,
     CLERK_PUBLISHABLE_KEY:process.env.CLERK_PUBLISHABLE_KEY,
     CLERK_SECRET_KEY:process.env.CLERK_SECRET_KEY,
     STREAM_API_KEY:process.env.STREAM_API_KEY,
